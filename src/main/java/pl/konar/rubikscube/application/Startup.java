008ad0c7d21489129675a45c268e69710b3434ac
Kamil Rosinski <kamil.rosinski@capgemini.com>
2016-05-24 08:28:27 +0200

    Application startup created

--- src/main/java/pl/konar/rubikscube/application/Startup.java
+++ src/main/java/pl/konar/rubikscube/application/Startup.java
@@ -1,26 +1,34 @@
 package pl.konar.rubikscube.application;
-	
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Startup extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+
+	private static final String BUNDLE_PATH = "pl/konar/rubikscube/bundle/base";
+	private static final String FXML_PATH = "pl/konar/rubikscube/view/solver.fxml";
+	private static final String APPLICATION_TITLE = "Rubik's Cube Solver";
+
 	public static void main(String[] args) {
-		launch(args);
+		Application.launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+		primaryStage.setTitle(APPLICATION_TITLE);
+		URL resource = getClass().getClassLoader().getResource(FXML_PATH);
+		ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_PATH);
+		Parent root = FXMLLoader.load(resource, bundle);
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
 }
---
+++ src/main/java/pl/konar/rubikscube/controller/CubeSolverController.java
@@ -0,0 +1,20 @@
+package pl.konar.rubikscube.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CubeSolverController {
+	
+	@FXML
+	private URL location;
+	
+	@FXML
+	private ResourceBundle resources;
+	
+	@FXML
+	private Label myLabel;
+
+}
