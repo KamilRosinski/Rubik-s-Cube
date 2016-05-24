a35cf5979fa62e1c17256cba625464d6866ce559
Kamil Rosinski <kamil.rosinski@capgemini.com>
2016-05-24 08:31:52 +0200

    Application title moved to bundle

--- src/main/java/pl/konar/rubikscube/application/Startup.java
+++ src/main/java/pl/konar/rubikscube/application/Startup.java
@@ -14,7 +14,6 @@ public class Startup extends Application {
 
 	private static final String BUNDLE_PATH = "pl/konar/rubikscube/bundle/base";
 	private static final String FXML_PATH = "pl/konar/rubikscube/view/solver.fxml";
-	private static final String APPLICATION_TITLE = "Rubik's Cube Solver";
 
 	public static void main(String[] args) {
 		Application.launch(args);
@@ -22,9 +21,9 @@ public class Startup extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws IOException {
-		primaryStage.setTitle(APPLICATION_TITLE);
 		URL resource = getClass().getClassLoader().getResource(FXML_PATH);
 		ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_PATH);
+		primaryStage.setTitle(bundle.getString("application.title"));
 		Parent root = FXMLLoader.load(resource, bundle);
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
