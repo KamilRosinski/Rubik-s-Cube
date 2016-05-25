bdfc80caaae9d2e0b46f81674604937f79476c64
Kamil Rosinski <kamil.rosinski@capgemini.com>
2016-05-25 09:59:49 +0200

    54 buttons added to view

--- src/main/java/pl/konar/rubikscube/controller/CubeSolverController.java
+++ src/main/java/pl/konar/rubikscube/controller/CubeSolverController.java
@@ -6,11 +6,20 @@ import java.util.ResourceBundle;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import pl.konar.rubikscube.model.RubiksCubeModel;
 
 public class CubeSolverController {
 
+	private static final double BUTTON_SIZE = 30;
+	private static final int NUMBER_OF_ROWS = 3;
+	private static final int NUMBER_OF_COLUMNS = 3;
+	private static final int NUMBER_OF_WALLS = 6;
+	private static final int[] FACETS_ORDER = { 5, 14, 23, 32, 41, 50, 38, 2, 6, 29, 20, 8, 4, 11, 42, 13, 40, 33, 24,
+			31, 22, 15, 44, 53, 51, 35, 26, 47, 49, 17, 39, 1, 10, 37, 30, 3, 21, 9, 28, 19, 12, 7, 45, 52, 16, 43, 54,
+			36, 27, 34, 48, 25, 46, 18 };
+	private static final int[] WALL_X_OFFSETS = { 1, 0, 1, 2, 3, 1 };
+	private static final int[] WALL_Y_OFFSETS = { 0, 1, 1, 1, 1, 2 };
 	private RubiksCubeModel model = new RubiksCubeModel();
 
 	@FXML
@@ -20,20 +29,37 @@ public class CubeSolverController {
 	private ResourceBundle resources;
 
 	@FXML
-	private Label myLabel;
-
-	@FXML
-	private Button myButton;
+	private GridPane cubeLayout;
 
 	@FXML
 	private void initialize() {
-//		myButton.setStyle("-fx-background-color: TRANSPARENT");
-		myButton.styleProperty().bind(Bindings.concat("-fx-background-color: ", model.getFacetColour(0)));
+		for (int wall = 0; wall < NUMBER_OF_WALLS; ++wall) {
+			for (int row = 0; row < NUMBER_OF_COLUMNS; ++row) {
+				for (int column = 0; column < NUMBER_OF_ROWS; ++column) {
+					Button button = new Button();
+					initializeButton(button, wall, row, column);
+					cubeLayout.add(button, NUMBER_OF_ROWS * WALL_X_OFFSETS[wall] + column,
+							NUMBER_OF_COLUMNS * WALL_Y_OFFSETS[wall] + row);
+				}
+			}
+		}
 	}
 
-	@FXML
-	public void buttonAction() {
-		model.setNextColour(0);
+	private void initializeButton(Button button, int wall, int row, int column) {
+		button.setMinSize(10, 10);
+		button.setMaxSize(50, 50);
+		button.setPrefSize(BUTTON_SIZE, BUTTON_SIZE);
+		int facetNumber = FACETS_ORDER[NUMBER_OF_ROWS * NUMBER_OF_COLUMNS * wall
+				+ NUMBER_OF_COLUMNS * row + column] - 1;
+		button.setOnAction(event -> model.setNextColour(facetNumber));
+		bindButtonToFacet(button, facetNumber);
+	}
+
+	private void bindButtonToFacet(Button button, int buttonFacetNumber) {
+		button.styleProperty()
+				.bind(Bindings.concat(
+						"-fx-border-color: black; -fx-border-radius: 4px; -fx-background-insets: 2; -fx-background-radius: 4px; -fx-background-color: ",
+						model.getFacetColour(buttonFacetNumber)));
 	}
 
 }
--- src/main/java/pl/konar/rubikscube/model/RubiksCubeModel.java
+++ src/main/java/pl/konar/rubikscube/model/RubiksCubeModel.java
@@ -8,47 +8,21 @@ import javafx.beans.property.SimpleObjectProperty;
 
 public class RubiksCubeModel {
 
-	private static final int NUMBER_OF_FACETS_PER_FACE = 9;
-	// private ObservableList<ObservableStringValue> colours =
-	// FXCollections.observableArrayList(new
-	// ArrayList<ObservableStringValue>());
-	// private ListProperty<ObservableStringValue> facets = new
-	// SimpleListProperty<ObservableStringValue>();
-
+	private static final int NUMBER_OF_FACETS = 54;
 	private List<ObjectProperty<Colour>> facetColours = new ArrayList<>();
 
 	public RubiksCubeModel() {
-		for (Colour colour : Colour.values()) {
-			for (int i = 0; i < NUMBER_OF_FACETS_PER_FACE; ++i) {
-				facetColours.add(new SimpleObjectProperty<Colour>(colour));
-//				facetColours.add(new SimpleStringProperty(colour.toString()));
-				// colours.add(new
-				// SimpleStringProperty(colour.getColourName()));
-			}
+		for (int i = 0; i < NUMBER_OF_FACETS; ++i) {
+			facetColours.add(new SimpleObjectProperty<Colour>(Colour.TRANSPARENT));
 		}
-		// facets.set(colours);
 	}
 
 	public ObjectProperty<Colour> getFacetColour(int index) {
 		return facetColours.get(index);
 	}
 
-	// public ObservableList<ObservableStringValue> getFacets() {
-	// return facets.get();
-	// }
-
-	// public ListProperty<ObservableStringValue> facetsProperty() {
-	// return facets;
-	// }
-
 	public void setNextColour(int index) {
 		facetColours.get(index).set(facetColours.get(index).get().getNextColour());
-//		ObservableStringValue oldColour = facetColours.get(index);
-//		String newColour = Colour.valueOf(oldColour.get()).getNextColour().toString();
-//		facetColours.get(index).set(newColour);
-//		System.err.println(facetColours.get(0));
-		// colours.set(index,
-		// Colour.valueOf(colours.get(index).toString().toUpperCase()).getNextColour().getColourName());
 	}
 
 }
